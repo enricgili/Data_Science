b8824b5796cc4554a17eb7e004ffbcbf0b893a28
enricgili <enric.gili@gmail.com>
2015-05-05 13:24:42 -0700

    Created week1 markdown

--- Coursera_3_Getting_and_Cleaning_Data/week_1/getdata014_Q1_4.R
+++ Coursera_3_Getting_and_Cleaning_Data/week_1/getdata014_Q1_4.R
@@ -1,7 +1,7 @@
 library(XML)
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
-download.file(fileUrl,destfile = "restos.xml",method="curl")
-doc <- xmlTreeParse("restos.xml",useInternal=TRUE)
+download.file(fileUrl,destfile = "restaurants.xml",method="curl")
+doc <- xmlTreeParse("restaurants.xml",useInternal=TRUE)
 rootNode <- xmlRoot(doc)
 ziplist <- as.numeric(xpathSApply(rootNode,"//zipcode",xmlValue)) #find all nodes called 'zipcode'
 length(ziplist[ziplist==21231])
